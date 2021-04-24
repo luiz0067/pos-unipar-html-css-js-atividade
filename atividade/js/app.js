3f19f17e64ce086ee8b0eb165b5d82def02d93a1
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-04-24 12:34:54 -0300

    Update app.js

--- atividade/js/app.js
+++ atividade/js/app.js
@@ -1,1 +1,53 @@
 
+
+$(document).ready(function (){
+    
+    var formNoticias = $("#form-noticias");
+    formNoticias.on("submit", function(){
+        try {
+            var json = recordFromForm(formNoticias);
+            addDataTable(json);
+        } catch (e){
+            console.error(e);
+        }
+        return false;
+    });
+
+    function recordFromForm(form){
+        var inputs = form.find('input[type="text"], textarea');
+        var json = "";
+        inputs.each(function(idx, input){
+            var name = $(input).attr("name");
+            var value = $(input).val();
+            if (json !== "")
+                json += ",";
+            
+            json += `"${name}": "${ value.trim() }"`;
+            console.log(json);
+        });
+        json = `{${json}}`;
+        return JSON.parse(json);
+    }
+
+	$(".remove").on("click",
+    function removeDataTable(){
+		$(this).closest("tr").remove();
+		var count_records= $("#table-noticias tbody").find("tr").length;
+        $("#total_registros").html(count_records);
+	});
+    
+    function addDataTable(noticiajson){
+        var tbody = $("#table-noticias tbody");
+        var tr = $("<tr></tr>");
+        var tdTitulo = $("<td></td>");   
+        var tdIntroducao = $("<td></td>");
+        var tdAcao = $("<td><a style='color:red' class='remove'>X<a></td>");
+        tdTitulo.text(noticiajson['titulo']);
+        tdIntroducao.text(noticiajson['introducao']);
+        tr.append(tdTitulo, tdIntroducao,tdAcao);
+        tbody.append(tr);
+        var count_records= $("#table-noticias tbody").find("tr").length;
+        $("#total_registros").html(count_records);
+    }
+
+});
